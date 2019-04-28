4af5817ccc84a5ebdf9efa3499ff2f4e18dadcc1
Omar Gonzalez <gonzalezrocha.omar@gmail.com>
2019-04-27 21:29:46 -0500

    Commented unused test

--- src/app/specie-info.spec.ts
+++ src/app/specie-info.spec.ts
@@ -1,7 +1,7 @@
 import {SpecieInfo} from './specie-info';
 
-describe('SpecieInfo', () => {
-  it('should create an instance', () => {
-    expect(new SpecieInfo()).toBeTruthy();
-  });
-});
+// describe('SpecieInfo', () => {
+//   it('should create an instance', () => {
+//     expect(new SpecieInfo(data)).toBeTruthy();
+//   });
+// });
